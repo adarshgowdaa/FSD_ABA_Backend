069d77eb3afc9c53a658ca0fd111d17c0ef0b77f
adarshgowdaa <adarshgowda463@gmail.com>
2023-07-02 18:51:01 +0530

    Added GET

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/routes.js';
-import DBConnection from './database/db.js';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import router from "./routes/routes.js";
+import DBConnection from "./database/db.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -11,10 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', router);
+app.use("/", router);
 
 const PORT = process.env.PORT;
 
 DBConnection();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!, The server is running :)");
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
